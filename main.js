329c8148df30432b06d1abff071c4c7b7337cd49
Your Nameart-abdullahi <abdullahihassanke@gmail.com>
2020-08-06 15:55:58 +0300

    ft: mobile responsive

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("hello");
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function container() {
+  document.getElementById("myContainer").classList.toggle("container-colomn");
+}
